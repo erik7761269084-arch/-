4b5bd07d0a86ad1720faf5607d5f60ff8ca31712
erik7761269084 <erik7761269084@gmail.com>
2025-09-28 23:12:21 +0800

    1

---
+++ video/heibaiju_data.js
@@ -0,0 +1,17 @@
+const seriesData = {
+  title: "黑白局",
+  status: "第20集完結",
+  category: "大陸電視劇",
+  director: "趙浚凱, 侯傑",
+  actors: "楊旭文, 王硯輝",
+  meta: "大陸 / 漢語普通話 / 2025",
+  description: "展鵬（楊旭文 飾）的父親是一名鐵路警察，多年前因公殉職...",
+  episodes: [
+    { number: 1, url: "https://cdn.wlcdn88.com:777/d1154710/index.m3u8" },
+    { number: 2, url: "https://cdn.wlcdn88.com:777/24a79096/index.m3u8" },
+    { number: 3, url: "https://cdn.wlcdn88.com:777/876c7a10/index.m3u8" },
+    { number: 4, url: "https://cdn.wlcdn88.com:777/08e571a4/index.m3u8" },
+    // ... 可以继续加
+  ]
+};
+ 
