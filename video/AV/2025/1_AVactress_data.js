55e33410ad7ef273761e1e8a715e4723ed4565fd
erik7761269084 <erik7761269084@gmail.com>
2025-10-11 14:18:49 +0800

    1

---
+++ video/AV/2025/1_AVactress_data.js
@@ -0,0 +1,20 @@
+const actressData = {
+  id: 1,
+  name: "美園和花",
+  age: 26,
+  birthday_date: "1999-03-03",
+  img_url: "https://picture.yoshiclub.xyz/20230329/d61cd24b-698e-4599-9cbb-64d9f57ea752.jpg",
+  background_img: "https://picture.yoshiclub.xyz/20241116/b50846ef-cd6c-428c-a3a2-b85baf967265.jpg",
+  zodiac: "雙魚座",
+  height: "155cm",
+  measurements: "胸 100 腰 60 臀 91",
+  cup: "G",
+  description: "美园和花(美園和花)是诞生于香川县的日本艺人...",
+  features: ["美人", "美乳", "大乳房", "大眼睛", "美少女", "長相清純", "長相甜美", "極上巨乳"],
+  detail_url: "https://www.javrate.com/Actor/Detail/16a72d89-d0f7-4ee8-b68c-8ef510114462.html",
+  aliases: ["Waka Misono", "Yui Yada", "矢田結衣", "美园和花"],
+  gallery: [
+    "https://picture.yoshiclub.xyz/GFriends/Content/w-Fitch/AI-Fix-美園和花.jpg",
+    "https://picture.yoshiclub.xyz/20230329/4586273d-237f-4ecf-a682-2a71142e9267.jpg"
+  ]
+};
