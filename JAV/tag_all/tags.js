55300f395dcf7233eee503dff2fd0bf8edf1ba65
erik7761269084 <erik7761269084@gmail.com>
2025-12-03 03:31:35 +0800

    1

---
+++ JAV/tag_all/tags.js
@@ -0,0 +1,24 @@
+// tags.js
+
+const tags = {
+    "乱交": "https://yunvgong.com/JAV/tag_all/tag_promiscuity",
+    "素人": "https://yunvgong.com/JAV/tag_all/tag_Amateur",
+    "巨乳": "https://yunvgong.com/JAV/tag_all/tag_bigBreasts",
+    "嫩妹": "https://yunvgong.com/JAV/tag_all/tag_youngGirl",
+    "少妇": "https://yunvgong.com/JAV/tag_all/tag_youngWoman",
+    "出道作": "https://yunvgong.com/JAV/tag_all/tag_DebutWork",
+    "高清画质": "https://yunvgong.com/JAV/tag_all/tag_HD",
+    "痴汉": "https://yunvgong.com/JAV/tag_all/tag_pervert",
+    "女同": "https://yunvgong.com/JAV/tag_all/tag_Lesbian",
+    "淫妻": "https://yunvgong.com/JAV/tag_all/tag_adulterousWife",
+    "乱伦": "https://yunvgong.com/JAV/tag_all/tag_incest",
+    "人妻": "https://yunvgong.com/JAV/tag_all/tag_wife",
+    "孕妇": "https://yunvgong.com/JAV/tag_all/tag_pregnantWomen",
+    "奶妈": "https://yunvgong.com/JAV/tag_all/tag_wetNurse"
+};
+
+// ✅ ES Module 导出
+export default tags;
+
+// ✅ CommonJS 导出 (如果 Node.js 环境使用 require)
+module.exports = tags;
