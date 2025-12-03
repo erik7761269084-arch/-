186709bc68b2a62ee9f74e54bde337d168f133f4
erik7761269084 <erik7761269084@gmail.com>
2025-12-03 10:43:28 +0800

    1

---
+++ JAV/actress_menu/7684_AVactress_data.js
@@ -0,0 +1,38 @@
+// 7684_AVactress_data.js
+window.actressData = {
+  "id": 7684,
+  "name": "凛酱",
+  "age": null,
+  "birthday_date": null,
+  "img_url": "https://yunvgong.com/model.jpg",
+  "background_img": "https://picture.yoshiclub.xyz/20230930/20230930013717874874.jpg",
+  "zodiac": null,
+  "height": "158cm",
+  "measurements": "86-57-87",
+  "measurements_display": "86-57-87F",
+  "cup": "F",
+  "description": null,
+  "features": [],
+  "detail_url": null,
+  "aliases": [],
+  "gallery": [],
+  "works_count": 1,
+  "related_dramas": [
+    {
+      "id": 141448,
+      "IDcode": "FC2-PPV-4548288",
+      "title": "庆祝20岁凛酱148cm赤壁简单JD凛酱有一分低品味一明确的问题秘密视觉频率一件快乐花园秘密视觉频率一小身体第三个谜团",
+      "year": "",
+      "country": "",
+      "genre": "",
+      "type": "",
+      "company": "FC2",
+      "publish_date": "2025-07-13",
+      "cover_url": "https://thjpg10.top/upload/vod/20250127-1/666f8d0c8aa3cb30200573f9e0c5235a.jpg",
+      "actress": [
+        "凛酱"
+      ],
+      "title_play_url": "https://yunvgong.com/JAV/player_template.html?series=FC2-PPV-4548288"
+    }
+  ]
+};
---
+++ JAV/actress_menu/7688_AVactress_data.js
@@ -0,0 +1,38 @@
+// 7688_AVactress_data.js
+window.actressData = {
+  "id": 7688,
+  "name": "娜娜酱",
+  "age": null,
+  "birthday_date": null,
+  "img_url": "https://yunvgong.com/model.jpg",
+  "background_img": "https://picture.yoshiclub.xyz/20230930/20230930013717874874.jpg",
+  "zodiac": null,
+  "height": "158cm",
+  "measurements": "86-57-87",
+  "measurements_display": "86-57-87F",
+  "cup": "F",
+  "description": null,
+  "features": [],
+  "detail_url": null,
+  "aliases": [],
+  "gallery": [],
+  "works_count": 1,
+  "related_dramas": [
+    {
+      "id": 140598,
+      "IDcode": "FC2-PPV-4550015",
+      "title": "娜娜酱脸第2弹高品质美巨乳大学生娜娜酱感觉太好了我们甚至约定下次见面第二次拍摄时肛门发育",
+      "year": "",
+      "country": "",
+      "genre": "",
+      "type": "",
+      "company": "FC2",
+      "publish_date": "2025-07-16",
+      "cover_url": "https://thjpg10.top/upload/vod/20250123-1/3c195fbf00ebe73bb0d79681148574b1.jpg",
+      "actress": [
+        "娜娜酱"
+      ],
+      "title_play_url": "https://yunvgong.com/JAV/player_template.html?series=FC2-PPV-4550015"
+    }
+  ]
+};
