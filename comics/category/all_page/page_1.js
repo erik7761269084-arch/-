f6c1f8d4ad943e9bdb0bfa97210b6129576ba58e
erik7761269084 <erik7761269084@gmail.com>
2025-12-05 17:00:07 +0800

    1

--- comics/category/all_page/page_1.js
+++ comics/category/all_page/page_1.js
@@ -1,38 +1,33 @@
-// 每个对象包含：title、cover、url、pageSeq
+// 每个对象包含：title、cover、url
 const PAGE_DATA = [
     {
         "IDcode": 1,
         "title": "[23.4ド (イチリ)] ボクの理想の異世界生活 10",
         "cover": "https://telegra.ph/file/57a648b769eebaaf7319f.jpg",
-        "url": "https://yunvgong.com/comics/comicDetails.html?series=1",
-        "pageSeq": 1
+        "url": "https://yunvgong.com/comics/comicDetails.html?series=1"
     },
     {
         "IDcode": 2,
         "title": "[かつおぶし (ホリエ)] 嵌事",
         "cover": "https://telegra.ph/file/bf8b2d359de0d844439dd.jpg",
-        "url": "https://yunvgong.com/comics/comicDetails.html?series=2",
-        "pageSeq": 2
+        "url": "https://yunvgong.com/comics/comicDetails.html?series=2"
     },
     {
         "IDcode": 3,
         "title": "[舞原マツゲ] NTR脱出ゲーム (COMIC ペンギンクラブ山賊版 2020年3月号)",
         "cover": "https://telegra.ph/file/479fa878695a0e643d7e7.jpg",
-        "url": "https://yunvgong.com/comics/comicDetails.html?series=3",
-        "pageSeq": 3
+        "url": "https://yunvgong.com/comics/comicDetails.html?series=3"
     },
     {
         "IDcode": 4,
         "title": "[Twitter] 奶凍 [Naidong] (2021.2.28-2022.11.05)",
         "cover": "https://telegra.ph/file/7aed07d608d94952e7418.jpg",
-        "url": "https://yunvgong.com/comics/comicDetails.html?series=4",
-        "pageSeq": 4
+        "url": "https://yunvgong.com/comics/comicDetails.html?series=4"
     },
     {
         "IDcode": 5,
         "title": "[さいもん] 曇りガラスの向こうの中で (初恋時間。)",
         "cover": "https://telegra.ph/file/b83f488ab23a46bb1a41b.jpg",
-        "url": "https://yunvgong.com/comics/comicDetails.html?series=5",
-        "pageSeq": 5
+        "url": "https://yunvgong.com/comics/comicDetails.html?series=5"
     }
 ];
