8c8a77f82faad63ced127480d6e1b754eb8f20b8
erik7761269084 <erik7761269084@gmail.com>
2025-12-05 16:36:35 +0800

    1

---
+++ comics/menu/3_comics_data.js
@@ -0,0 +1,29 @@
+const comicData = {
+    "IDcode": 3,
+    "title": "[舞原マツゲ] NTR脱出ゲーム (COMIC ペンギンクラブ山賊版 2020年3月号)",
+    "sourceUrl": "https://telegra.ph/舞原マツゲ-NTR脱出ゲーム-COMIC-ペンギンクラブ山賊版-2020年3月号-08-31",
+    "images": [
+        "https://telegra.ph/file/479fa878695a0e643d7e7.jpg",
+        "https://telegra.ph/file/056b0e264e5a19e47ea96.jpg",
+        "https://telegra.ph/file/2e3b906a6cc673aef9dea.jpg",
+        "https://telegra.ph/file/4d3f29b997a58ad1ed48b.jpg",
+        "https://telegra.ph/file/2f57cd0311823af033388.jpg",
+        "https://telegra.ph/file/41482e75c01b268b880ae.jpg",
+        "https://telegra.ph/file/f2d038edd161afbc31019.jpg",
+        "https://telegra.ph/file/62266f256cb1ec2a87957.jpg",
+        "https://telegra.ph/file/3bc78ab9941270ef2a459.jpg",
+        "https://telegra.ph/file/c555101d1cce03bd3d74c.jpg",
+        "https://telegra.ph/file/f199d879cec3a12baf4c2.jpg",
+        "https://telegra.ph/file/8cda49c384113ed6a5f37.jpg",
+        "https://telegra.ph/file/e1a4c699c9319b038ddda.jpg",
+        "https://telegra.ph/file/e210f6811987c7ed4982e.jpg",
+        "https://telegra.ph/file/b0e8f72dd5a21a9c52433.jpg",
+        "https://telegra.ph/file/63438ab24af617d66e480.jpg",
+        "https://telegra.ph/file/b3ce1338c580a0738e73a.jpg",
+        "https://telegra.ph/file/74a2e6b7ea26199e50c4d.jpg",
+        "https://telegra.ph/file/da21850f4c2c57e0d4720.jpg",
+        "https://telegra.ph/file/d7ff616038fb1c7e4b776.jpg",
+        "https://telegra.ph/file/5d3004397ba1f12ef2698.jpg",
+        "https://telegra.ph/file/d61b1cce0b2de14e5ad6b.jpg"
+    ]
+};
---
+++ comics/menu/5_comics_data.js
@@ -0,0 +1,23 @@
+const comicData = {
+    "IDcode": 5,
+    "title": "[さいもん] 曇りガラスの向こうの中で (初恋時間。)",
+    "sourceUrl": "https://telegra.ph/さいもん-曇りガラスの向こうの中で-初恋時間-08-31",
+    "images": [
+        "https://telegra.ph/file/b83f488ab23a46bb1a41b.jpg",
+        "https://telegra.ph/file/cea9cc593ba6cf890afef.jpg",
+        "https://telegra.ph/file/b7568183f69b89473a42d.jpg",
+        "https://telegra.ph/file/09c2543683e7b1eafc8a6.jpg",
+        "https://telegra.ph/file/5d1102f521a0369b62502.jpg",
+        "https://telegra.ph/file/1b2fe720767f6e798f1d2.jpg",
+        "https://telegra.ph/file/439d3298ee5259d2b0f3d.jpg",
+        "https://telegra.ph/file/5ce79c508137ce78f1eaa.jpg",
+        "https://telegra.ph/file/cf5a730283f2c6c013acb.jpg",
+        "https://telegra.ph/file/c9f267ae14bd5e35a77ca.jpg",
+        "https://telegra.ph/file/e86ec8e6cd15e47bd49b3.jpg",
+        "https://telegra.ph/file/dad885f48da46ebd80523.jpg",
+        "https://telegra.ph/file/a6908c2d9326def320cb2.jpg",
+        "https://telegra.ph/file/b2767749dc3aadb84c028.jpg",
+        "https://telegra.ph/file/ea1b3eae9c69af0513e4e.jpg",
+        "https://telegra.ph/file/40dcfc0c047d2277d91ad.jpg"
+    ]
+};
