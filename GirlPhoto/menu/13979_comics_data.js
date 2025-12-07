c5b35312af25e03e909f5b5831a742d58d9c0d4c
erik7761269084 <erik7761269084@gmail.com>
2025-12-07 19:31:08 +0800

    1

--- GirlPhoto/menu/13979_comics_data.js
+++
@@ -1,9 +0,0 @@
-const comicData = {
-    "IDcode": 13979,
-    "title": "Vol.589 周于希Sandy",
-    "sourceUrl": "https://telegra.ph/Vol589-周于希Sandy-08-30",
-    "images": [],
-    "genres": [
-        "彩色"
-    ]
-};
--- GirlPhoto/menu/13984_comics_data.js
+++
@@ -1,9 +0,0 @@
-const comicData = {
-    "IDcode": 13984,
-    "title": "Vol.625 周于希Sandy",
-    "sourceUrl": "https://telegra.ph/Vol625-周于希Sandy-08-30-2",
-    "images": [],
-    "genres": [
-        "彩色"
-    ]
-};
