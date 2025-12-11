a0cb8cc92c9f410419a47f0413abb7846f583cd7
erik7761269084 <erik7761269084@gmail.com>
2025-12-11 18:37:48 +0800

    1

--- schoolGirl/category/all_page/page_2.js
+++ schoolGirl/category/all_page/page_2.js
@@ -3,7 +3,7 @@ window.PAGE_DATA = [
     {
         "IDcode": 101,
         "title": "御女宫少女阁 少女九十九",
-        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B399%2072p%20+%203v/0.jpg",
+        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B399%2072p%20+%203v/1%20(2).jpg",
         "url": "https://yunvgong.com/schoolGirl/comicDetails.html?series=101",
         "genres": [
             "彩色"
@@ -12,7 +12,7 @@ window.PAGE_DATA = [
     {
         "IDcode": 102,
         "title": "御女宫少女阁 少女一百",
-        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3100%2027p%20+%206v/0.jpg",
+        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3100%2027p%20+%206v/IMG_20250904_014322_176.jpg",
         "url": "https://yunvgong.com/schoolGirl/comicDetails.html?series=102",
         "genres": [
             "彩色"
@@ -21,7 +21,7 @@ window.PAGE_DATA = [
     {
         "IDcode": 103,
         "title": "御女宫少女阁 少女一百一",
-        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3101%2022p%20+%2014v/0.jpg",
+        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3101%2022p%20+%2014v/1%20(1).jpg",
         "url": "https://yunvgong.com/schoolGirl/comicDetails.html?series=103",
         "genres": [
             "彩色"
@@ -30,7 +30,7 @@ window.PAGE_DATA = [
     {
         "IDcode": 104,
         "title": "御女宫少女阁 少女一百二",
-        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3102%2038p%20+%202v/1.jpg",
+        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3102%2038p%20+%202v/2%20(2).jpg",
         "url": "https://yunvgong.com/schoolGirl/comicDetails.html?series=104",
         "genres": [
             "彩色"
@@ -39,7 +39,7 @@ window.PAGE_DATA = [
     {
         "IDcode": 105,
         "title": "御女宫少女阁 少女一百三",
-        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3103%2018p%20+%208v/5.jpg",
+        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3103%2018p%20+%208v/IMG_20250904_014911_466.jpg",
         "url": "https://yunvgong.com/schoolGirl/comicDetails.html?series=105",
         "genres": [
             "彩色"
@@ -48,7 +48,7 @@ window.PAGE_DATA = [
     {
         "IDcode": 106,
         "title": "御女宫少女阁 少女一百四",
-        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3104%2037p%20+3v/1.jpg",
+        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3104%2037p%20+3v/2%E9%BB%84%E8%A1%A3%20(2).jpg",
         "url": "https://yunvgong.com/schoolGirl/comicDetails.html?series=106",
         "genres": [
             "彩色"
@@ -57,17 +57,8 @@ window.PAGE_DATA = [
     {
         "IDcode": 107,
         "title": "御女宫少女阁 少女一百五",
-        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3105%2025p%20+%2052v/0%20(1).jpg",
+        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3105%2025p%20+%2052v/5%E6%88%BF%E9%97%B4%20(6).jpg",
         "url": "https://yunvgong.com/schoolGirl/comicDetails.html?series=107",
-        "genres": [
-            "彩色"
-        ]
-    },
-    {
-        "IDcode": 108,
-        "title": "御女宫少女阁 少女一百六",
-        "cover_url": "",
-        "url": "https://yunvgong.com/schoolGirl/comicDetails.html?series=108",
         "genres": [
             "彩色"
         ]
@@ -75,7 +66,7 @@ window.PAGE_DATA = [
     {
         "IDcode": 109,
         "title": "御女宫少女阁 少女一百七",
-        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3107%2028p%20+%206v/1.jpg",
+        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3107%2028p%20+%206v/4%20(5).jpg",
         "url": "https://yunvgong.com/schoolGirl/comicDetails.html?series=109",
         "genres": [
             "彩色"
@@ -84,7 +75,7 @@ window.PAGE_DATA = [
     {
         "IDcode": 110,
         "title": "御女宫少女阁 少女一百八",
-        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3108%2050p/3.jpg",
+        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3108%2050p/2%E8%83%B8%20(1).jpg",
         "url": "https://yunvgong.com/schoolGirl/comicDetails.html?series=110",
         "genres": [
             "彩色"
@@ -93,7 +84,7 @@ window.PAGE_DATA = [
     {
         "IDcode": 111,
         "title": "御女宫少女阁 少女一百九",
-        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3109%2051p%20+%2013v/0.jpg",
+        "cover_url": "https://erik7761269084-arch.github.io/japanencoxada/%E5%B0%91%E5%A5%B3%E9%98%81%E9%A2%91%E9%81%93/%E5%B0%91%E5%A5%B3109%2051p%20+%2013v/2%20(4).jpg",
         "url": "https://yunvgong.com/schoolGirl/comicDetails.html?series=111",
         "genres": [
             "彩色"
